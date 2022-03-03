b3ced55527603e9ddd3d5be6570e2603cdf0b8bd
valivia <valivia@xayania.com>
2022-03-03 21:42:15 +0000

    fixed scroll

--- components/imageItem.tsx
+++ components/imageItem.tsx
@@ -12,7 +12,7 @@ export default function ImageItem(data: GalleryImage): JSX.Element {
 
 
   return (
-    <Link href={`/project/${data.project_uuid}`} scroll={true}>
+    <Link href={`/project/${data.project_uuid}`}>
       <a className={styles.main}>
         <div className={styles.info}>
           <h3>{data.name}</h3>
