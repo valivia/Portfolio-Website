9ca2401746c0a0a1c50b5cd529cb41eecd706866
valivia <valivia@xayania.com>
2022-05-17 22:51:10 +0000

    about page markdown

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,8 +9,11 @@ import skillData from "../public/skills.json";
 import { SkillCategory } from "../types/types";
 import { GetStaticProps } from "next";
 import Footer from "../components/footer.module";
-import Link from "next/link";
 import MailingList from "../components/mailing.module";
+
+import { MDXProvider } from "@mdx-js/react";
+import AboutMarkdown from "../public/markdown/about.mdx";
+import SkillsetMarkdown from "../public/markdown/skillset.mdx";
 
 class About extends React.Component<Props, State> {
   private scroll() {
@@ -37,30 +40,10 @@ class About extends React.Component<Props, State> {
 
           <article className={styles.textbox}>
             <header>About me</header>
-            <section>
-              <h1>aaaaa</h1>
-              <p>
-                Expedita iste cum velit ipsum aut qui. Tempora tempora nihil omnis animi possimus ut non quia. Praesentium ratione natus temporibus consequatur
-                doloremque et voluptates. Aut odio animi deserunt voluptatem veritatis non et deserunt. Excepturi consequuntur explicabo vitae officia.
-              </p>
-              <p>
-                Placeat et quos qui voluptatem quibusdam voluptatem ad dolorem. Tempore mollitia voluptates id. Est maxime ea omnis explicabo est alias cupiditate. Et aspernatur quibusdam consequuntur ut.
-              </p>
-              <p>
-                Natus quia facilis sed. Velit odio eum in iusto aut. Labore aut sequi tempora incidunt eos ea veritatis. Numquam qui quia et sit voluptates laboriosam eum. Et voluptatibus quia totam perspiciatis.
-              </p>
-              <h3>hobbies</h3>
-              <p>
-                Ratione veritatis modi fuga quam beatae. Laborum debitis et et. Et sint minus eaque quisquam non mollitia animi.
-              </p>
-              <p>
-                Animi natus culpa veritatis corrupti voluptatum. Modi est voluptas nemo. Illo totam dolores beatae quia aut et cum. Vero vitae qui quas. Sunt perferendis nihil dolores atque consequatur.
-              </p>
-              <ul>
-                <li><Link href="/browse#art"><a>art</a></Link></li>
-                <li><Link href="/browse#github"><a>github</a></Link></li>
-                <li><Link href="/projects"><a>projects</a></Link></li>
-              </ul>
+            <section className={styles.markdown}>
+              <MDXProvider>
+                <AboutMarkdown />
+              </MDXProvider>
             </section>
             <figure className={styles.aboutPic}>
               <Image
@@ -78,24 +61,10 @@ class About extends React.Component<Props, State> {
               <section className={styles.skills}>
                 <SkillList skills={this.props.skills} />
               </section>
-              <section>
-                <h2>Skills</h2>
-                <p>
-                  Expedita iste cum velit ipsum aut qui. Tempora tempora nihil omnis animi possimus ut non quia. Praesentium ratione natus temporibus consequatur
-                  doloremque et voluptates. Aut odio animi deserunt voluptatem veritatis non et deserunt. Excepturi consequuntur explicabo vitae officia.
-                </p>
-                <p>
-                  Placeat et quos qui voluptatem quibusdam voluptatem ad dolorem. Tempore mollitia voluptates id. Est maxime ea omnis explicabo est alias cupiditate. Et aspernatur quibusdam consequuntur ut.
-                </p>
-                <p>
-                  Natus quia facilis sed. Velit odio eum in iusto aut. Labore aut sequi tempora incidunt eos ea veritatis. Numquam qui quia et sit voluptates laboriosam eum. Et voluptatibus quia totam perspiciatis.
-                </p>
-                <p>
-                  Ratione veritatis modi fuga quam beatae. Laborum debitis et et. Et sint minus eaque quisquam non mollitia animi.
-                </p>
-                <p>
-                  Animi natus culpa veritatis corrupti voluptatum. Modi est voluptas nemo. Illo totam dolores beatae quia aut et cum. Vero vitae qui quas. Sunt perferendis nihil dolores atque consequatur.
-                </p>
+              <section className={styles.markdown}>
+                <MDXProvider>
+                  <SkillsetMarkdown />
+                </MDXProvider>
               </section>
             </section>
           </article>
